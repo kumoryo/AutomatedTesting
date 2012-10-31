15250eb85060678ad4898f972d7af0d435b096c4
kumoryo <314004856@qq.com>
2012-10-31 22:13:30 +0800

    test

---
+++ GuidBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by 晓川 on 12-10-31.
+//  Copyright (c) 2012年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
